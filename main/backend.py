765a124cbbc24f1b0bb91ee44005b147514d8425
JacobGordon43 <jacobg43@cox.net>
2024-03-31 12:38:00 -0700

    Backend Work
    Gets the user's ID when logging in, started work on getting user form data

--- main/backend.py
+++ main/backend.py
@@ -2,6 +2,7 @@ import requests
 import aws
 import json
 import actions
+import datetime
 def login(username, password):
     url = aws.login_url
     headers = {"Content-Type": "application/json"}
@@ -12,16 +13,42 @@ def login(username, password):
     print(response.status_code)
     if response.status_code == 200:
         data = json.loads(data)
+        userId = response.json()["userId"]
+        print(response.json())
+        print(userId)
         with open("./json/login.json", 'w') as file: json.dump(data, file, indent=4)
         with open("./json/create_account.json", 'w') as file: json.dump(data, file, indent=4)
-    return response.status_code
+        with open("./json/userId.json", 'w') as file: json.dump({"userId": userId}, file, indent=4)
 
+    return response.status_code
+# login("jacobg1@cox.net", "Password43!")
 def get_data():
-    data = actions.handle_file('./json/userId.json')
+    userId = actions.handle_file('./json/userId.json')
+    print(userId["userId"])
+    # data = {            
+    #     "domainName": "EZApply Desktop",
+    #     "time": datetime.datetime.now(),
+    #     "body": {
+    #         "tableName": "personalFormData",
+    #         "expectsOne": True,
+    #         userId: userId
+    #     }
+    # }
+    
+    # Create new lambda function to handle this request
+    data = {
+        "body": json.dumps({
+            "message": "sent"
+        }, default=str)
+    }
+    print(data)
     # Adds additional key value pairs to send to the API
-    url = aws.login_url
+    url = aws.data_url
     headers = {"Content-Type": "application/json"}
     # TODO create a separate call for each table, add a key value in data for that table
     # TODO create an additional file for determining how each call will handle it's data in formatting to the JSON format for this application
     # TODO create a separate function for each file. Specify which function is called based on the database.
-    results = requests.post(url=url, data=data)
+    results = requests.post(url=url, data=data, headers=headers)
+    print(json.loads(results.text))
+    print(results.status_code)
+get_data()
